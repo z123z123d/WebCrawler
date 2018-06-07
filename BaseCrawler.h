e209ff4f9218eaa8a161d414b74d1572926c4fd4
z123z123d <1114941413@qq.com>
2018-06-07 15:52:05 +0800

    Update BaseCrawler.h
    更新了init()接口的参数

--- BaseCrawler.h
+++ BaseCrawler.h
@@ -11,12 +11,11 @@ class BaseCrawler {
 /*
     ObjectList is the list of all the downloaded movies
     TargetDataList is the list of all the targeted data
-
 */
 
     virtual void addTargetData (const DataType &) = 0; // add a kind of target data
-    virtual void addObject (const BaseObject* &) = 0; // add a object(movie)
+    virtual void addObject (const BaseObject* &) = 0; // add an object(movie)
 public:
-    virtual void init (const vector<DataType> &) = 0; // the general interface of initialization
+    virtual void init() = 0; // the general interface of initialization
     virtual void work() = 0; // the general interface of main work
 };
